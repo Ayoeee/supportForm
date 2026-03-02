e2c0f70008f6402bbd3bbfbc767d1229d8c5d608
Ayoeee <ayobami.elutade.ext@kinship.co>
2026-03-02 16:24:28 +0100

    removed unsued code in spec

--- tests/fillSupportForm/fillForm.spec.js
+++ tests/fillSupportForm/fillForm.spec.js
@@ -2,7 +2,4 @@ const { test, expect } = require('../../fixtures/test-fixtures')
 
 test('Associates can fill support form', async ({ fillSupportFormFlow }) => {
   await fillSupportFormFlow.fillSupportFormWithoutUploadForBugRelatedIssues()
-  // await fillSupportFormFlow.assertAssociateCreation()
-  // await fillSupportFormFlow.deleteAssociate()
-  // await fillSupportFormFlow.assertAssociateDeletion()
 })
